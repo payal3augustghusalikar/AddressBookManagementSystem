6447b3893530940dc09fbd774d045bdec8f88d49
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-11-26 16:40:32 +0530

    Added : Welcome message

---
+++ com/addressbookmanagement/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.addressbookmanagement;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("********* Welcome to Address Book Management System *********");
+    }
+}
